f83b4fabb2ee814531a134c60381428ed0b31bec
Giwon <68110047+whitedev7773@users.noreply.github.com>
2025-03-22 23:24:17 +0900

    feat: 섹션2 개발 & Time 디스플레이 개발 (#3)

---
+++ src/lib/components/remaining_time/DateBox.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Text from '$lib/components/Text.svelte';
+	import DateBoxHeader from './DateBoxHeader.svelte';
+
+	export let label: string = '';
+	export let display_number: number = 0;
+
+	const numberList = Array.from({ length: 60 }, (_, i) => i);
+</script>
+
+<div class="datebox">
+	<DateBoxHeader text={label} />
+	<div class="datebox-content" style={`transform: translateY(-${display_number * 114}px)`}>
+		{#each numberList as number}
+			<div class="datebox-content-number">
+				<Text fontSize="45px" fontWeight="700" color="black">{number}</Text>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	div.datebox {
+		width: 120px;
+		height: 148px;
+		background-color: #fff;
+		border-radius: 10px;
+		overflow: hidden;
+	}
+
+	div.datebox-content {
+		z-index: 0;
+		position: relative;
+		overflow: hidden;
+
+		transition: transform 0.25s ease-out;
+	}
+
+	div.datebox-content-number {
+		z-index: 0;
+		width: 100%;
+		height: 114px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
---
+++ src/lib/components/remaining_time/DateBoxHeader.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Text from '$lib/components/Text.svelte';
+
+	export let text: string = '';
+</script>
+
+<div>
+	<Text fontSize="14px" fontWeight="600" textAlign="center">
+		{text}
+	</Text>
+</div>
+
+<style>
+	div {
+		position: relative;
+		z-index: 10;
+
+		width: 100%;
+		height: 34px;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		background-color: #50b97d;
+	}
+</style>
